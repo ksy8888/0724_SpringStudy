96a0cccb02cafc19cca37212127c68a434d7b791
ksy8888 <ksy8_13@naver.com>
2023-07-27 09:00:33 +0900

    DI_food페이징처리_0726

---
+++ SpringDIProject_5_0725/src/main/java/com/sist/dao/SeoulDAO.java
@@ -0,0 +1,49 @@
+package com.sist.dao;
+import java.util.*;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
+public class SeoulDAO extends SqlSessionDaoSupport {
+	/*
+	 <select id="seoulListData" resultType="SeoulLocationVO" parameterType="hashmap">
+   SELECT no,title,address,num
+   FROM (SELECT no,title,address,rownum as num
+   FROM (SELECT no,title,address 
+   FROM seoul_location ORDER BY no ASC))
+   WHERE num BETWEEN #{start} AND #{end}
+   <!--  #{start} map.get("start") 
+         Map map = new HashMap()
+         map.put("start",1)
+         map.put("end",10)
+   -->
+  </select>
+	 */
+	public List<SeoulLocationVO> seoulListData(Map map) {
+		return getSqlSession().selectList("seoulListData",map); //seoulListData에 map의 값을 넣어서 실행
+	}
+	
+	/*
+	  <select id="seoulTotalPage" resultType="int">
+       SELECT CEIL(COUNT(*)/10.0) FROM seoul_location
+      </select>
+	 */
+	public int seoulTotalPage() {
+		return getSqlSession().selectOne("seoulTotalPage");
+	}
+	
+	/*
+	 <select id="seoulFindData" resultType="SeoulLocationVO" parameterType="String">
+      SELECT no,title,address,msg
+      FROM seoul_location
+      WHERE title LIKE '%'||#{title}||'%'
+    </select>
+	*/
+	public List<SeoulLocationVO> seoulFindData(String title) {
+		return getSqlSession().selectList("seoulFindData",title);
+	}
+	
+	public List<FoodVO> FoodListData() {
+		return getSqlSession().selectList("foodListData");
+	}
+	
+}
